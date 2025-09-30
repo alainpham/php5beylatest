f805cc7ed924538b7a5c825c155dcb106530d541
apham <admin@zez.duckdns.org>
2025-10-01 00:15:55 +0200

    with profiling

--- api.php
+++ api.php
@@ -1,23 +1,37 @@
 <?php
 header("Content-Type: application/json");
 
-$dsn = "pgsql:host=pgdb;port=5432;dbname=testdb;";
-$user = "user";
-$pass = "password";
+// $dsn = "pgsql:host=pgdb;port=5432;dbname=testdb;";
+// $user = "user";
+// $pass = "password";
 
-try {
-    $pdo = new PDO($dsn, $user, $pass);
-} catch (PDOException $e) {
-    http_response_code(500);
-    echo json_encode(["error" => "DB Connection failed"]);
-    exit;
-}
+// try {
+//     $pdo = new PDO($dsn, $user, $pass);
+// } catch (PDOException $e) {
+//     http_response_code(500);
+//     echo json_encode(["error" => "DB Connection failed"]);
+//     exit;
+// }
+// $pdo->exec("
+//     CREATE TABLE IF NOT EXISTS users (
+//         id SERIAL PRIMARY KEY,
+//         name VARCHAR(100),
+//         email VARCHAR(100)
+//     );
+// ");
+
+// $pdo->exec("
+//     INSERT INTO users (name, email) VALUES
+//     ('Alice', 'alice@example.com'),
+//     ('Bob', 'bob@example.com')
+//     ON CONFLICT DO NOTHING;
+// ");
 
 $request = $_SERVER['REQUEST_URI'];
 
 if ($request === '/api/users') {
-    $stmt = $pdo->query("SELECT * FROM users");
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // $stmt = $pdo->query("SELECT * FROM users");
+    // $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     doDummyStuff();
 
@@ -26,11 +40,11 @@ if ($request === '/api/users') {
     doSomeOtherDummyStuff();
     
     $ip = json_decode($resp, true)['origin'];
-    foreach ($users as &$user) {
-        $user['ip'] = $ip;
-    }
+    // foreach ($users as &$user) {
+    //     $user['ip'] = $ip;
+    // }
     unset($user);
-    echo json_encode($users);
+    echo json_encode($resp);
 } else {
     http_response_code(404);
     echo json_encode(["error" => "Not Found"]);
